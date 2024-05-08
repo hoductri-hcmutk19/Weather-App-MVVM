aa09f778c103d8ca30af0383540d8f2a878c6107
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-05-08 16:49:41 +0700

    splash screen

---
+++ app/src/main/java/com/example/weather/ui/splash/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.example.weather.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather.ui.MainActivity
+import com.example.weather.utils.Constant
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private val mHandler = Handler(Looper.getMainLooper())
+    private lateinit var mRunnable: Runnable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mRunnable = Runnable {
+            goToMainActivity()
+        }
+        mHandler.postDelayed(mRunnable, Constant.SPLASH_TIME)
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            .also { intent ->
+                startActivity(intent)
+            }
+    }
+
+    override fun onDestroy() {
+        mHandler.removeCallbacks(mRunnable)
+        super.onDestroy()
+    }
+}
