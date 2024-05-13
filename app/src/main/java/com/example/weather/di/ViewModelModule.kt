2ba8075230cb6b1c95c1ae1a53759575ebcf4ace
hoductrihcmut123 <tri.ho_dt_bd@hcmut.edu.vn>
2024-05-13 11:32:02 +0700

    Map screen

--- app/src/main/java/com/example/weather/di/ViewModelModule.kt
+++ app/src/main/java/com/example/weather/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.example.weather.di
 import com.example.weather.ui.MainViewModel
 import com.example.weather.ui.SharedViewModel
 import com.example.weather.ui.home.WeatherViewModel
+import com.example.weather.ui.map.MapViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -11,4 +12,5 @@ val ViewModelModule: Module = module {
     viewModel { MainViewModel() }
     viewModel { SharedViewModel() }
     viewModel { WeatherViewModel(get()) }
+    viewModel { MapViewModel(get()) }
 }
